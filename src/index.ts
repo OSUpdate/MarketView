b8f1a16064a7b3594753a3be2901833f4f5579cd
OSUpdate <force185@naver.com>
2020-12-24 18:05:32 +0900

    init

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { Context } from 'koa'; 
+const Koa = require('koa');
+const app = new Koa(); 
+app.use((ctx: Context) => {
+     ctx.body = 'hello, Jacob!'; 
+    }); 
+app.listen(4000, () => { 
+    console.log('Listening to port 4000'); 
+});
